0ed2bc01ea9832c412a9e1e8720fa1231043c541
cameronwp <cameron.w.pittman@gmail.com>
2015-12-07 19:00:52 -0800

    adds instructions

--- site/app/scripts/app.js
+++ site/app/scripts/app.js
@@ -1,8 +1,15 @@
-;var loader = (function(document) {
+/*
+Instructions:
+(1) Wrap an XHR in a Promise in the get() function below. See: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+  (a) Resolve on load and reject on error.
+(2) If the XHR resolves, use addSearchHeader to add the search header to the page.
+(3) If the XHR fails, console.log the error.
+ */
+
+(function(document) {
   'use strict';
 
   var home = null;
-  var planetInfos = [];
 
   /*
   Helper function to show the search query.
@@ -11,88 +18,16 @@
     home.innerHTML = '<h2 class="page-title">query: ' + string + '</h2>';
   };
 
-  /*
-  Helper function to create thumbnails
-   */
-  function createPlanetThumb(data) {
-    planetInfos.push({name: data.pl_name, data: data});
-    // var pM = document.createElement('paper-material');
-    // pM.elevation = '1';
-    var pT = document.createElement('planet-thumb');
-    for (let d in data) {
-      pT[d] = data[d];
-    }
-    // pM.appendChild(pT);
-    home.appendChild(pT);
-  }
-
-  function get(url) {
-    // Return a new promise.
-    return new Promise(function(resolve, reject) {
-      // Do the usual XHR stuff
-      var req = new XMLHttpRequest();
-      req.open('GET', url);
-
-      req.onload = function() {
-        // This is called even on 404 etc
-        // so check the status
-        if (req.status == 200) {
-          // Resolve the promise with the response text
-          resolve(req.response);
-        }
-        else {
-          // Otherwise reject with the status text
-          // which will hopefully be a meaningful error
-          reject(Error(req.statusText));
-        }
-      };
-
-      // Handle network errors
-      req.onerror = function() {
-        reject(Error("Network Error"));
-      };
-
-      // Make the request
-      req.send();
-    });
-  }
-
-  function getJSON(url) {
-    return get(url).then(JSON.parse);
-  };
-
-  function loadSequence() {
-    // Proj: Part 1 code goes here!
-    home = document.querySelector('section[data-route="home"]');
-    return getJSON('/data/earth-like-results.json')
-      .then(function(d) {
-        addSearchHeader(d.query);
-        return d.results;
-      })
-      .then(function(urls) {
-        urls.reduce(function (sequence, planetURL) {
-          return sequence.then(function () {
-            return getJSON(planetURL);
-          })
-          .then(function(planetData) {
-            createPlanetThumb(planetData)
-          });
-        }, Promise.resolve())
-      })
-      .then(function() {
-        // Proj: this will need to be added before migrating loading logic to routing.html
-        return planetInfos;
-      })
+  function get() {
+    /*
+    Your Promise goes here!
+     */
   };
 
   window.addEventListener('WebComponentsReady', function() {
-    // Proj: Uncomment for start of project
-    // loadSequence();
+    /*
+    Uncomment the next line you're ready to start chaining and testing!
+     */
+    // get('http://udacity.github.io/exoplanet-explorer/site/app/data/earth-like-results.json');
   });
-
-  // Proj: add when moving routing logic to routing.html
-  return {
-    getJSON: getJSON,
-    loadSequence: loadSequence
-  }
 })(document);

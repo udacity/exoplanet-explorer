41537cb4171691fd30b295eb30145d206142fc9c
cameronwp <cameron.w.pittman@gmail.com>
2015-12-09 19:45:35 -0800

    adds comment about bug

--- site/app/scripts/app.js
+++ site/app/scripts/app.js
@@ -55,6 +55,8 @@ Instructions:
     /*
     Your code goes here! Uncomment the next line when you're ready to start!
      */
+    // TODO: swap this out with a non local source!!!
+    // Not seeming to be in series?
     getJSON('../data/earth-like-results.json')
     .then(function(response) {
       var sequence = Promise.resolve();

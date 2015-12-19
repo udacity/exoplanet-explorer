78ffaed33bc40aafabe3325a97d8bcaafcf85dc4
cameronwp <cameron.w.pittman@gmail.com>
2015-12-19 14:21:38 -0800

    adds poster images

---
+++ utils/compress-images.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x1200 -compress JPEG ../app/images/Final_Peg_51_Poster_9x14.jpg ../app/images/Peg_51-1200.jpg;
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x600 -compress JPEG ../app/images/Final_Peg_51_Poster_9x14.jpg ../app/images/Peg_51-600.jpg;
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x300 -compress JPEG ../app/images/Final_Peg_51_Poster_9x14.jpg ../app/images/Peg_51-300.jpg;
+
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x1200 -compress JPEG ../app/images/HD_40307g_39x27_screen_small.jpg ../app/images/HD_40307g-1200.jpg;
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x600 -compress JPEG ../app/images/HD_40307g_39x27_screen_small.jpg ../app/images/HD_40307g-600.jpg;
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x300 -compress JPEG ../app/images/HD_40307g_39x27_screen_small.jpg ../app/images/HD_40307g-300.jpg;
+
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x1200 -compress JPEG ../app/images/Kepler_16b_screen_small.jpg ../app/images/Kepler_16b-1200.jpg;
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x600 -compress JPEG ../app/images/Kepler_16b_screen_small.jpg ../app/images/Kepler_16b-600.jpg;
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x300 -compress JPEG ../app/images/Kepler_16b_screen_small.jpg ../app/images/Kepler_16b-300.jpg;
+
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x1200 -compress JPEG ../app/images/Kepler_186f_screen_small.jpg ../app/images/Kepler_186f-1200.jpg;
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x600 -compress JPEG ../app/images/Kepler_186f_screen_small.jpg ../app/images/Kepler_186f-600.jpg;
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x300 -compress JPEG ../app/images/Kepler_186f_screen_small.jpg ../app/images/Kepler_186f-300.jpg;
+
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x1200 -compress JPEG ../app/images/PSOJ318.5-22_screen.jpg ../app/images/PSOJ318.5-22-1200.jpg;
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x600 -compress JPEG ../app/images/PSOJ318.5-22_screen.jpg ../app/images/PSOJ318.5-22-600.jpg;
+convert -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 10000x300 -compress JPEG ../app/images/PSOJ318.5-22_screen.jpg ../app/images/PSOJ318.5-22-300.jpg;

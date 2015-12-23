c6db0005a1ca797dc22af24b1d9b05b124281e11
cameronwp <cameron.w.pittman@gmail.com>
2015-12-23 14:43:10 -0800

    fixes jshint errors

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,7 +18,7 @@ Instructions:
    */
   function addSearchHeader(query) {
     home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
-  };
+  }
 
   /**
    * Helper function to create a planet thumbnail.
@@ -26,11 +26,11 @@ Instructions:
    */
   function createPlanetThumb(data) {
     var pT = document.createElement('planet-thumb');
-    for (let d in data) {
+    for (var d in data) {
       pT[d] = data[d];
     }
     home.appendChild(pT);
-  };
+  }
 
   /**
    * XHR wrapped in a promise.
@@ -40,8 +40,8 @@ Instructions:
   function get(url) {
     return fetch(url, {
       method: 'get'
-    })
-  };
+    });
+  }
 
   /**
    * Performs an XHR for a JSON and returns a parsed JSON response.
@@ -52,7 +52,7 @@ Instructions:
     return get(url).then(function(response) {
       return response.json();
     });
-  };
+  }
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
